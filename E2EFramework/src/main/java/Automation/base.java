432a11c3fe53f5cc6925c19cdd5a5980c88b58d1
Vusal <Vusal@192.168.0.31>
2021-03-20 00:27:22 +0100

    Some other changes to E2E Framework desing

--- E2EFramework/src/main/java/Automation/base.java
+++ E2EFramework/src/main/java/Automation/base.java
@@ -1,6 +1,7 @@
 package Automation;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -12,22 +13,22 @@ public class base {
 	
 	public WebDriver driver;
 
-	public WebDriver driverInitializer() 
+	public WebDriver driverInitializer() throws IOException 
 	{
-		Properties prop = Properties();
+		Properties prop = new Properties();
 		FileInputStream fis = new FileInputStream("C:\\Users\\Vusal\\git\\repository\\E2EFramework\\src\\main\\java\\Automation\\data.properties");
 		
 		prop.load(fis);
 		
 		String browserName = prop.getProperty("browser");
 		
-		if(browserName == "Chrome")
+		if(browserName.equals("Chrome"))
 		{
 			System.setProperty("webdriver.chrome.driver", "C:\\Users\\Vusal\\OneDrive\\Desktop\\1\\SoftWareTesting\\exeFiles\\chromedriver.exe");
 			driver = new ChromeDriver();
 			
 		}
-		else if (browserName == "IE")
+		else if (browserName.equals("IE"))
 		{
 			System.setProperty("webdriver.ie.driver", "C:\\Users\\Vusal\\OneDrive\\Desktop\\1\\SoftWareTesting\\exeFiles\\msedgedriver.exe");  
 			driver=new InternetExplorerDriver();
--- E2EFramework/src/test/java/Automation/HomePage.java
+++ E2EFramework/src/test/java/Automation/HomePage.java
@@ -1,11 +1,13 @@
 package Automation;
 
+import java.io.IOException;
+
 import org.testng.annotations.Test;
 
 public class HomePage extends base {
 	
 	@Test
-	public void basePageNavigation()
+	public void basePageNavigation() throws IOException
 	{
 		driver = driverInitializer();
 		driver.get("https://www.google.com/");
